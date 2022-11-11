40fa6675be9cef439951a780e5ad4ed171dc2659
Erik Martinez <93367385+erkmartinezs@users.noreply.github.com>
2022-11-10 22:28:16 -0800

    create sources and model with name fake_model

---
+++ models/fake_model/fake_model.sql
@@ -0,0 +1,15 @@
+{{ config(materialized='table') }}
+
+with src_dd as(
+
+    SELECT  *
+    FROM {{source('wh_dummie','dummies_data')}}
+)
+
+, final as(
+    SELECT  *
+    FROM    src_dd
+)
+
+SELECT  *
+FROM    final
